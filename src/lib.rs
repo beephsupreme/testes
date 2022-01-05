7e56994a18f9758b513dd92e94e8c99192ab0e29
Michael Rowsey <beephsupreme@gmail.com>
2022-01-05 15:53:14 -0800

    add gcd, fib, lcm, bubble sort, tests for bubble sort

--- src/lib.rs
+++ src/lib.rs
@@ -7,3 +7,86 @@ pub fn bubble_sort<T: Ord>(arr: &mut [T]) {
         }
     }
 }
+
+pub fn gcd(mut m: i32, mut n: i32) -> i32 {
+    let mut m_prime;
+    loop {
+        m_prime = m % n;
+        if m_prime == 0 {
+            break;
+        }
+        m = n;
+        n = m_prime;
+    }
+    n
+}
+
+pub fn lcm(m: i32, n: i32) -> i32 {
+    let g = gcd(m, n);
+    m * n / g
+}
+
+pub fn fibonacci(n: u64) -> u64 {
+    if n == 0 || n == 1 {
+        return n;
+    }
+    let mut p: u64 = 0;
+    let mut c: u64 = 1;
+    for _ in 0..n - 1 {
+        let t: u64 = c;
+        c = p + c;
+        p = t;
+    }
+    c
+}
+
+#[cfg(test)] // Only compiles when running tests
+mod tests {
+    // Separates tests from code
+    use super::bubble_sort;
+
+    fn comp_vec(u: Vec<i32>, v: Vec<i32>) -> bool {
+        if u.len() != v.len() {
+            return false;
+        }
+        for i in 0..u.len() {
+            if u[i] != v[i] {
+                return false;
+            }
+        }
+        true
+    }
+
+    #[test]
+    fn test_in_order_bubble_sort() {
+        let t = vec![1, 2, 3, 4, 5, 6, 7, 8, 9];
+        let mut x = vec![1, 2, 3, 4, 5, 6, 7, 8, 9];
+        bubble_sort(&mut x);
+        let are_equal = comp_vec(t, x);
+        assert_eq!(true, are_equal);
+    }
+    #[test]
+    fn test_reverse_order_bubble_sort() {
+        let t = vec![1, 2, 3, 4, 5, 6, 7, 8, 9];
+        let mut x = vec![9, 8, 7, 6, 5, 4, 3, 2, 1];
+        bubble_sort(&mut x);
+        let are_equal = comp_vec(t, x);
+        assert_eq!(true, are_equal);
+    }
+    #[test]
+    fn test_random_order_bubble_sort() {
+        let t = vec![1, 2, 3, 4, 5, 6, 7, 8, 9];
+        let mut x = vec![3, 8, 2, 6, 9, 7, 4, 1, 5];
+        bubble_sort(&mut x);
+        let are_equal = comp_vec(t, x);
+        assert_eq!(true, are_equal);
+    }
+    #[test]
+    fn test_not_equal_bubble_sort() {
+        let t = vec![1, 2, 3, 4, 5, 6, 7, 8, 9];
+        let mut x = vec![3, 8, 2, 6, 5, 7, 4, 1, 5];
+        bubble_sort(&mut x);
+        let are_equal = comp_vec(t, x);
+        assert_ne!(true, are_equal);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,5 @@
 fn main() {
-    let mut v: Vec<i32> = vec![5, 2, 4, 1, 3];
-    mnr::bubble_sort(&mut v);
-    println!("{:?}", v);
-
-    let mut strings = ["beach", "hotel", "airplane", "car", "house", "art"];
-    mnr::bubble_sort(&mut strings);
-    println!("{:?}", strings);
+    println!("{}", mnr::gcd(60, 24));
+    println!("{}", mnr::lcm(60, 24));
+    println!("{}", mnr::fibonacci(93))
 }
