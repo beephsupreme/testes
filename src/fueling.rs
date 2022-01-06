375d5f0a5b4e7fd46cc29a2d25d470bf4fbbe58a
Michael Rowsey <beephsupreme@gmail.com>
2022-01-06 08:34:39 -0800

    fix mod import

--- src/fueling.rs
+++ src/fueling.rs
@@ -1,25 +1,23 @@
-mod fueling {
 
-    fn compute_minimum_stops(distance: i32, range: i32, checkpoints: Vec<i32>)-> i32 {
-        let mut refills = 0;
-        let base_range = range;
-        for i in 0..checkpoints.len() {
-            if i == n {
-                return refills;
-            } else if i + 1 > range{
-                return -1;
+pub fn compute_minimum_stops(mut range: i32, checkpoints: &mut Vec<i32>) -> i32 {
+    let mut refills = 0;
+    let base_range = range;
+    for mut i in 0..checkpoints.len() {
+        if i == checkpoints.len() {
+            return refills;
+        } else if i + 1 > range as usize {
+            return -1;
+        } else {
+            while i + 1usize <= range as usize && i != checkpoints.len() {
+                i += 1usize;
+            }
+            if i == checkpoints.len() {
+                continue;
             } else {
-                while i+1 <= range && i != checkpoints.len() {
-                    i += 1;
-                }
-                if i == checkpoints.len() {
-                    continue;
-                } else {
-                    refills += 1;
-                    range = base_range + checkpoints[i];
-                }
+                refills += 1;
+                range = base_range + checkpoints[i];
             }
         }
-        refills
     }
+    refills
 }
--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,6 @@ mod fueling;
 
 fn main() {
     let mut v = vec![200, 350, 550, 900];
-    fueling::compute_minimum_stops(900, 400, &mut v);
+    println!("{}", fueling::compute_minimum_stops(2, &mut v));
+    
 }
