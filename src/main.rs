5c5aec79576105a5a2ca555624429660f9144c26
Michael Rowsey <mike@rowsey.org>
2022-01-07 01:37:48 -0800

    add segment class

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,47 @@
-#[derive(Debug)]
-struct Segment {
-    x: i32,
-    y: i32,
-}
-
-impl Segment {
-    fn new(x: i32, y: i32) -> Segment {
-        Segment { x: x, y: y }
-    }
-   fn hello(&self) {
-        println!("hello");
-    }
-    fn chicken(&self) {
-        println!("butt");
-    }
-}
-
+mod segments;
+use segments::Segment;
 
 fn main() {
-    let x = 32;
-    let y = 42;
-    let s: Segment::new(1 as i32, 2);
-    s.hello();
-    s.chicken();
+    let s: Segment = Segment::new(42 , 61);
+    let t: Segment = Segment::new(12 , 62);
+    println!("is {} < {}? {}", 
+        s.get_y(),
+        t.get_y(),
+        segments::cmp(&s, &t));
 }
+
+
+// int main() {
+//     Segment *temp;
+//     vector<Segment *> lines(0);
+//     vector<INT> used(0);
+//     vector<INT> found(0);
+//     INT count{0}, left, right, n;
+//     cin >> n;
+//     for (int i = 0; i < n; ++i) {
+//         cin >> left >> right;
+//         temp = new Segment(left, right);
+//         lines.push_back(temp);
+//         used.push_back(0);
+//     }
+//     sort(lines.begin(), lines.end(), &cmp);
+//     for (int i = 0; i < lines.size(); ++i) {
+//         if (!used[i]) {
+//             for (int j = i + 1; j < lines.size(); ++j) {
+//                 if (lines[i]->intersects(*lines[j])) {
+//                     used[j] = 1;
+//                 } else {
+//                     used[i] = 1;
+//                 }
+//             }
+//             count++;
+//             found.push_back(lines[i]->getY());
+//         }
+//     }
+//     cout << count << endl;
+//     for (auto &f : found) {
+//         cout << f << " ";
+//     }
+//     cout << endl;
+//     return 0;
+// }
--- src/segments.rs
+++ src/segments.rs
@@ -1,17 +1,32 @@
 #[derive(Debug)]
 pub struct Segment {
-    pub x: i32,
-    pub y: i32,
+    x: i32,
+    y: i32,
 }
 
 impl Segment {
-    pub fn new(x: i32, y: i32) -> Segment {
+    pub fn new(mut x: i32, mut y: i32) -> Segment {
+        if x > y {
+            std::mem::swap(&mut x, &mut y);
+        }
         Segment { x: x, y: y }
     }
-    pub fn hello(&self) {
-        println!("hello");
+
+    pub fn get_y(&self) -> i32 {
+        self.y
     }
-    pub fn chicken(&self) {
-        println!("butt");
+
+    pub fn intersects(&self, obj: &Segment) -> bool {
+        if self.y < obj.x {
+            return false;
+        } else if self.x > obj.y {
+            return false;
+        } else {
+            return true;
+        }
     }
+}
+
+pub fn cmp(lhs: &Segment, rhs: &Segment) -> bool {
+    lhs.get_y() < rhs.get_y()
 }
