4c2a00a6416e27477157a52171ee061140eb353d
Michael Rowsey <beephsupreme@gmail.com>
2022-01-10 08:07:14 -0800

    change error messages in InputError

--- src/input.rs
+++ src/input.rs
@@ -4,12 +4,12 @@ use thiserror::Error;
 #[derive(Error, Debug)]
 #[non_exhaustive]
 pub enum InputError {
-    #[error("ArgLength error: Expected {0}, Found {1}.")]
+    #[error("ArgLength: Expected {0} values, Got {1}.")]
     ArgLengthError(usize, usize),
     #[error("Error reading from stdin.")]
     ReadError,
-    #[error("Error parsing input.")]
-    ParseError,
+    #[error("Could not parse {0}")]
+    ParseError(String),
 }
 
 pub fn get_int_vec(n: usize) -> Result<Vec<i32>, InputError> {
@@ -22,7 +22,7 @@ pub fn get_int_vec(n: usize) -> Result<Vec<i32>, InputError> {
         .split_whitespace()
         .map(|s| s.parse()).collect() {
         Ok(numbers) => { numbers }
-        Err(_) => { return Err(InputError::ParseError); }
+        Err(_) => { return Err(InputError::ParseError(input)); }
     };
     if n == numbers.len() {
         Ok(numbers)
